35dc5c91b7f105e34838d6f4f990f5ae31e2e34a
Bheutschi <87085780+Bheutschi@users.noreply.github.com>
2025-01-14 15:50:23 +0100

    refactor : add new refactor to learn some tricks (#8)
    * chore(phpstan): add the config file neon
    
    * chore(phpstan): install phpstan
    
    * refactor: add multiple function and add a setupPdf for more readability
    
    * refactor: new refactor to learn some tricks

--- src/GeneratePdf.php
+++ src/GeneratePdf.php
@@ -4,89 +4,16 @@ namespace TicketData;
 
 class GeneratePdf
 {
-    
-    public RequestGlpi $requestGlpi;
+    public SetupPdf $setupPdf;
 
-    public function __construct(RequestGlpi $requestGlpi)
+    public function __construct(SetupPdf $setupPdf)
     {
-        $this->requestGlpi = $requestGlpi;
-    }
-    
-    public function sortValueComputers() : array
-    {
-        $computers = $this->requestGlpi->fetchAllComputer();
-
-        return array_map(function ($array) {
-            $realNameContact = explode(".", $array['contact']);
-            return [
-                'name' => $array['name'],
-                'contact' => ucfirst($realNameContact[0]),
-                'uuid' => $array['uuid'],
-            ];
-        }, $computers);
+        $this->setupPdf = $setupPdf;
     }
 
-    public function handle(): void
+    public function generatePdfAllComputers(): void
     {
-        $apprentices = $this->sortValueComputers();
-
-        $tcPdf = new SetupPdf('P', 'mm', 'A4', true, 'UTF-8');
-        $tcPdf->setPrintHeader(false);
-        $tcPdf->setPrintFooter(false);
-        $tcPdf->setMargins(0, 0, 0);
-        $tcPdf->setAutoPageBreak(false);
-        $tcPdf->SetFont('helvetica', 'B', 10);
-
-        for ($page = 0; $page < $tcPdf->totalOfPages($apprentices); $page++) {
-            $tcPdf->AddPage();
-            $this->addStickersToPage($tcPdf, $apprentices);
-
-        }
-        $tcPdf->Output(__DIR__ . "/ec-pc-labels.pdf", 'F');
-    }
-
-    public function addSticker(SetupPdf $tcpdf, array $apprentices, float $current_column_offset, float $current_line_offset): void
-    {
-        $tcpdf->write2DBarcode(
-            $apprentices["uuid"],
-            'DATAMATRIX',
-            $current_column_offset + 6,
-            $current_line_offset + 2,
-            12,
-            12,
-            [
-                'border' => 0,
-                'vpadding' => 0,
-                'hpadding' => 0,
-                'fgcolor' => [0, 0, 0],
-                'bgcolor' => false,
-                'module_width' => 1,
-                'module_height' => 1,
-            ],
-            'N'
-        );
-        $tcpdf->Text($current_column_offset + 20, $current_line_offset + 2, $apprentices["name"]);
-        $tcpdf->Text($current_column_offset + 20, $current_line_offset + 8, $apprentices["contact"]);
-    }
-    
-    private function addStickersToPage(SetupPdf $tcPdf, array $apprentices): void
-    {
-        for ($line = 0; $line < $tcPdf->lines; $line++) {
-            $current_line_offset = $tcPdf->top_margin + ($line * $tcPdf->sticker_height);
-
-            for ($column = 0; $column < $tcPdf->columns; $column++) {
-                $current_column_offset = $tcPdf->left_margin + ($column * $tcPdf->sticker_width);
-
-                if ($tcPdf->sticker_count > $tcPdf->number_of_stickers_per_type) {
-                    $tcPdf->current_sticker++;
-                    if ($tcPdf->current_sticker >= count($apprentices)) {
-                        break;
-                    }
-                    $tcPdf->sticker_count = 1;
-                }
-                $this->addSticker($tcPdf, $apprentices[$tcPdf->current_sticker], $current_column_offset, $current_line_offset);
-                $tcPdf->sticker_count++;
-            }
-        }
+        $this->setupPdf->setPdf($this->setupPdf);
+        $this->setupPdf->Output(__DIR__ . "/ec-pc-labels.pdf", 'F');
     }
 }
--- src/SetupPdf.php
+++ src/SetupPdf.php
@@ -6,6 +6,7 @@ use TCPDF;
 
 class SetupPdf extends TCPDF
 {
+    public AdaptRequest $adaptRequest;
     public int $top_margin = 5;
     public int $left_margin = 0;
     public int $sticker_width = 70;
@@ -16,6 +17,13 @@ class SetupPdf extends TCPDF
     public int $current_sticker = 0;
     public int $number_of_stickers_per_type = 1;
 
+    public function __construct(AdaptRequest $adaptRequest)
+    {
+        parent::__construct();
+        $this->adaptRequest = $adaptRequest;
+    }
+
+
     public function stickersPerPages(): float|int
     {
         return $this->lines * $this->columns;
@@ -24,5 +32,66 @@ class SetupPdf extends TCPDF
     public function totalOfPages( array $apprentices): float
     {
         return ceil(($this->number_of_stickers_per_type * count($apprentices)) / $this->stickersPerPages());
+    }
+    public function setPdf(SetupPdf $tcpdf): SetupPdf
+    {
+        $apprentices = $this->adaptRequest->sortValueComputers();
+        $this->setPrintHeader(false);
+        $this->setPrintFooter(false);
+        $this->setMargins(0, 0, 0);
+        $this->setAutoPageBreak(false);
+        $this->SetFont('helvetica', 'B', 10);
+
+        for ($page = 0; $page < $this->totalOfPages($apprentices); $page++) {
+            $this->AddPage();
+            $this->addStickersToPage($tcpdf, $apprentices);
+
+        }
+        return $this;
+    }
+
+    public function addSticker(SetupPdf $tcpdf, array $apprentices, float $current_column_offset, float $current_line_offset): void
+    {
+        $tcpdf->write2DBarcode(
+            $apprentices["uuid"],
+            'DATAMATRIX',
+            $current_column_offset + 6,
+            $current_line_offset + 2,
+            12,
+            12,
+            [
+                'border' => 0,
+                'vpadding' => 0,
+                'hpadding' => 0,
+                'fgcolor' => [0, 0, 0],
+                'bgcolor' => false,
+                'module_width' => 1,
+                'module_height' => 1,
+            ],
+            'N'
+        );
+        $tcpdf->Text($current_column_offset + 20, $current_line_offset + 2, $apprentices["name"]);
+        $tcpdf->Text($current_column_offset + 20, $current_line_offset + 8, $apprentices["contact"]);
+    }
+
+    private function addStickersToPage(SetupPdf $tcpdf, array $apprentices): void
+    {
+        for ($line = 0; $line < $this->lines; $line++) {
+            $current_line_offset = $this->top_margin + ($line * $this->sticker_height);
+
+            for ($column = 0; $column < $this->columns; $column++) {
+                $current_column_offset = $this->left_margin + ($column * $this->sticker_width);
+
+                if ($this->sticker_count > $this->number_of_stickers_per_type) {
+                    $this->current_sticker++;
+                    if ($this->current_sticker >= count($apprentices)) {
+                        break;
+                    }
+                    $this->sticker_count = 1;
+                }
+                $this->addSticker($tcpdf, $apprentices[$this->current_sticker], $current_column_offset, $current_line_offset);
+                $this->sticker_count++;
+            }
+        }
     }
 }
