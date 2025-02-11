7129818cd0b4831f7f1e6dc7bc39d3ac88bc7489
bheutschi <bheutschi@gmail.com>
2025-02-11 16:19:59 +0100

    refactor: change the logic to how generate datamatrix

--- src/SetupPdf.php
+++ src/SetupPdf.php
@@ -2,96 +2,111 @@
 
 namespace TicketData;
 
+use InvalidArgumentException;
+use PhpParser\Node\Expr\Variable;
+use PHPStan\Analyser\RicherScopeGetTypeHelper;
+use Ramsey\Uuid\Uuid;
 use TCPDF;
 
 class SetupPdf extends TCPDF
 {
-    public AdaptRequest $adaptRequest;
-    public int $top_margin = 5;
-    public int $left_margin = 0;
-    public int $sticker_width = 70;
-    public float $sticker_height = 19.9;
-    public int $lines = 17;
-    public  $columns = 3;
-    public int $sticker_count = 1;
-    public int $current_sticker = 0;
+    private Sticker $sticker;
     public int $number_of_stickers_per_type = 1;
+    public int $size_of_datamatrix = 20;
+    public int $top_margin;
+    public int $left_margin;
+    public int $right_margin;
+    public int $space;
 
-    public function __construct(AdaptRequest $adaptRequest)
+    public function __construct(Sticker $sticker, $orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = 'UTF-8', $diskcache = false, $left_margin = 0, $right_margin = 0, $top_margin = 0,$space = 0)
     {
         parent::__construct();
-        $this->adaptRequest = $adaptRequest;
-    }
-
-
-    public function stickersPerPages(): float|int
-    {
-        return $this->lines * $this->columns;
+        $this->sticker = $sticker;
+        
+        $this->left_margin = $left_margin;
+        $this->right_margin = $right_margin;
+        $this->top_margin = $top_margin;
+        
+        $this->SetMargins($this->left_margin, $this->top_margin, $this->right_margin);
+        $this->space = $space;
     }
     
-    public function totalOfPages( array $apprentices): float
+    public function calculate_column($width_of_content): int
     {
-        return ceil(($this->number_of_stickers_per_type * count($apprentices)) / $this->stickersPerPages());
+        return floor((($this->getPageWidth() - ($this->right_margin + $this->left_margin)) / ($width_of_content + $this->space)));
     }
-    public function setPdf(SetupPdf $tcpdf): SetupPdf
+    
+    public function calculate_line($height_of_content): int
     {
-        $apprentices = $this->adaptRequest->sortValueComputers();
-        $this->setPrintHeader(false);
-        $this->setPrintFooter(false);
-        $this->setMargins(0, 0, 0);
-        $this->setAutoPageBreak(false);
-        $this->SetFont('helvetica', 'B', 10);
+        return floor((($this->getPageHeight() - $this->top_margin ) / ($height_of_content + $this->space)));
+    }
 
-        for ($page = 0; $page < $this->totalOfPages($apprentices); $page++) {
-            $this->AddPage();
-            $this->addStickersToPage($tcpdf, $apprentices);
+    public function generate_uuid(): string
+    {
+        $uuid = Uuid::uuid4()->toString();
+        return str_replace('-', '', $uuid);
+    }
 
+    public function ask_number_of_sticker_per_type(): void
+    {
+        echo "How many stickers per type do you want to generate ?\n";
+        $value = trim(fgets(STDIN));
+        $this->number_of_stickers_per_type = $value;
+    }
+
+    public function generateDataMatrix(array $array_of_x_and_y): void
+    {
+        foreach ($array_of_x_and_y as $array_position) {
+            $x = $array_position["x"];
+            $y = $array_position["y"];
+            $this->SetXY($x, $y);
+            $uuid = $array_position["uuid"] ?? $this->generate_uuid();
+            $this->write2DBarcode(
+                $uuid,
+                'DATAMATRIX',
+                $this->GetX(),
+                $this->GetY(),
+                $this->size_of_datamatrix,
+                $this->size_of_datamatrix,
+                [
+                    'border' => 0,
+                    'vpadding' => 0,
+                    'hpadding' => 0,
+                    'fgcolor' => [0, 0, 0],
+                    'bgcolor' => false,
+                    'module_width' => 1,
+                    'module_height' => 1,
+                ],
+                'N');
         }
-        return $this;
     }
-
-    public function addSticker(SetupPdf $tcpdf, array $apprentices, float $current_column_offset, float $current_line_offset): void
+    public function calculate_position($content_width, $content_height): array
     {
-        $tcpdf->write2DBarcode(
-            $apprentices["uuid"],
-            'DATAMATRIX',
-            $current_column_offset + 6,
-            $current_line_offset + 2,
-            12,
-            12,
-            [
-                'border' => 0,
-                'vpadding' => 0,
-                'hpadding' => 0,
-                'fgcolor' => [0, 0, 0],
-                'bgcolor' => false,
-                'module_width' => 1,
-                'module_height' => 1,
-            ],
-            'N'
-        );
-        $tcpdf->Text($current_column_offset + 20, $current_line_offset + 2, $apprentices["name"]);
-        $tcpdf->Text($current_column_offset + 20, $current_line_offset + 8, $apprentices["contact"]);
-    }
+        $positions = [];
+        $number_of_columns = $this->calculate_column($content_width);
+        $number_of_lines = $this->calculate_line($content_height);
 
-    private function addStickersToPage(SetupPdf $tcpdf, array $apprentices): void
-    {
-        for ($line = 0; $line < $this->lines; $line++) {
-            $current_line_offset = $this->top_margin + ($line * $this->sticker_height);
-
-            for ($column = 0; $column < $this->columns; $column++) {
-                $current_column_offset = $this->left_margin + ($column * $this->sticker_width);
-
-                if ($this->sticker_count > $this->number_of_stickers_per_type) {
-                    $this->current_sticker++;
-                    if ($this->current_sticker >= count($apprentices)) {
-                        break;
-                    }
-                    $this->sticker_count = 1;
+        $y = $this->top_margin;
+        for ($line = 0; $line < $number_of_lines; $line++) {
+            $x = $this->left_margin;
+            for ($column = 0; $column < $number_of_columns; $column++) {
+                
+                if ($x > (int)$this->getPageWidth() || $y > (int)$this->getPageHeight()) {
+                    continue;
                 }
-                $this->addSticker($tcpdf, $apprentices[$this->current_sticker], $current_column_offset, $current_line_offset);
-                $this->sticker_count++;
+                
+                $positions[] = [
+                    "x" => $x,
+                    "y" => $y,
+                ];
+                
+                $x += $this->space + $content_width;
             }
+            $y += $this->space + $content_height;
         }
+        
+        return $positions;
     }
+    
+    
 }
