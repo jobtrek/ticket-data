339babebc80f207b9268514906ce571a898dde2f
Bheutschi <87085780+Bheutschi@users.noreply.github.com>
2025-03-17 07:45:12 +0100

    chore: Change config and dependency files (#15)
    * chore: add the env.example
    
    * chore(dep): install larastan
    
    * chore: change the config file phpstan.neon
    
    * chore: add the glpi services.php

--- config/services.php
+++ config/services.php
@@ -34,5 +34,10 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'glpi' => [
+        'url' => env('GLPI_URL'),
+        'user_token' => env('USER_TOKEN'),
+        'app_token' => env('APP_TOKEN'),
+    ],
 
 ];
