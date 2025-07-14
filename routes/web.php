bd6726daf5666e0e87e075fa990dcd5cae3740ef
bheutschi <bheutschi@gmail.com>
2025-07-14 15:46:39 +0200

    chore: change the main root for the generate datamatrix

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Middleware\EnsureTokenIsSetup;
 use App\Services\TokenService;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TokenService::class, 'getSessionToken']);
+Route::get('/token', [TokenService::class, 'getSessionToken']);
 Route::resource('/computers', ComputerController::class)->middleware(EnsureTokenIsSetup::class);
-Route::get('/data', [DataMatrixController::class, 'index']);
+Route::get('/', [DataMatrixController::class, 'index']);
 Route::post('/data', [DataMatrixController::class, 'cacheNewNumber'])->name('data');
 Route::post('/deleteonedata', [DataMatrixController::class, 'deleteOneNumber'])->name('deleteonedata');
 Route::post('/generatedata', [DataMatrixController::class, 'generateDataMatrix'])->name('generatedata');
